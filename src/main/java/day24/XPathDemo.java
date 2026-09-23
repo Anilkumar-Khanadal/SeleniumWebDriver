1c96f53e5c98b702fd8316bc31887a654558d4ac
Anilkumar-Khanadal <108235039+Anilkumar-Khanadal@users.noreply.github.com>
2026-09-23 12:43:07 +0530

    ssa

--- src/main/java/day24/XPathDemo.java
+++ src/main/java/day24/XPathDemo.java
@@ -12,24 +12,32 @@ public class XPathDemo {
 		driver.manage().window().maximize();
 		
 		//xpath with single attributes
-		//driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("T-shirts");
+		driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("T-shirts");
 		
 		//xpath with multiple attributes
-		// driver.findElement(By.xpath("//input[@name='search'][@placeholder='Search']")).sendKeys("T-shirts");
+		driver.findElement(By.xpath("//input[@name='search'][@placeholder='Search']")).sendKeys("T-shirts");
 		
 		//xpath with 'and' 'or' operator
-		//driver.findElement(By.xpath("//input[@placeholder='Search' and @name='search']")).sendKeys("T-shirts");
-		//driver.findElement(By.xpath("//input[@placeholder='Search' or @name='search']")).sendKeys("T-shirts");
+		driver.findElement(By.xpath("//input[@placeholder='Search' and @name='search']")).sendKeys("T-shirts");
+		driver.findElement(By.xpath("//input[@placeholder='Search' or @name='search']")).sendKeys("T-shirts");
 		
 		//xpath with inner text - text()  (No attributes)
-		//driver.findElement(By.xpath("//a[text()='Desktops']")).click();
+		driver.findElement(By.xpath("//a[text()='Desktops']")).click();
 		
 		boolean displayStatus= driver.findElement(By.xpath("//h3[text()='Featured']")).isDisplayed();
 		System.out.println(displayStatus);
 		
 		String value = driver.findElement(By.xpath("//h3[text()='Featured']")).getText();
 		System.out.println(value);
+
+		//xpath with contains()
+		driver.findElement(By.xpath("//input[contains(@placeholder, 'Sea')]")).sendKeys("Tshirts");
 		
-		//01:12:00
+        //xpath with start-with()
+		driver.findElement(By.xpath("//input[starts-with(@placeholder, 'Sea)]")).sendKeys("TShirts");
+		
+        //chained xpath
+		boolean status = driver.findElement(By.xpath("//div[@id='logo']/a/img")).isDisplayed();
+		System.out.println(status);
 	}
 }
